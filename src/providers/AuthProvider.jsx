f19682e8b84c549886de3ac14e9f6772268c5c6d
Jecheon Ryu <rjc1704@gmail.com>
2025-11-25 17:20:00 +0900

    일부수정

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -28,8 +28,6 @@ export default function AuthProvider({ children }) {
   const [user, setUser] = useState(null);
   const router = useRouter();
 
-  const [isLoading, setIsLoading] = useState(true);
-
   const getUser = async () => {
     try {
       const userData = await userService.getMe();
@@ -83,26 +81,16 @@ export default function AuthProvider({ children }) {
       const token = await getServerSideToken();
       console.log("token::", token);
       if (token) {
-        getUser().then(() => {
-          setIsLoading(false);
-        });
+        getUser();
       } else {
-        setIsLoading(false);
+        setUser(null);
       }
     }
     fetchUser();
   }, []);
 
-  if (isLoading) {
-    return (
-      <div className="fixed inset-0 flex justify-center items-center bg-white/60 z-50">
-        <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-blue-500"></div>
-      </div>
-    );
-  }
-
   return (
-    <AuthContext.Provider value={{ user, login, logout, register, isLoading }}>
+    <AuthContext.Provider value={{ user, login, logout, register }}>
       {children}
     </AuthContext.Provider>
   );
